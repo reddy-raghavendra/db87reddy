2e2f2ad0de9f51d66e04dfbb1cd35a56b43e6a8a
Raghavendra Reddy <77422478+reddy-raghavendra@users.noreply.github.com>
2021-04-09 16:17:42 -0500

    Added put and get query

--- controllers/shoe.js
+++ controllers/shoe.js
@@ -11,8 +11,15 @@ exports.shoe_list = async function(req, res) {
 
 };
 // for a specific Costume.
-exports.shoe_detail = function(req, res) {
-    res.send('NOT IMPLEMENTED: Shoe detail: ' + req.params.id);
+exports.shoe_detail = async function(req, res) {
+    console.log("detail" + req.params.id)
+    try {
+        result = await Shoe.findById(req.params.id)
+        res.send(result)
+    } catch (error) {
+        res.status(500)
+        res.send(`{"error": document for id ${req.params.id} not found`);
+    }
 };
 // Handle Costume create on POST.
 exports.shoe_create_post = async function(req, res) {
@@ -37,8 +44,22 @@ exports.shoe_delete = function(req, res) {
     res.send('NOT IMPLEMENTED: Shoe delete DELETE ' + req.params.id);
 };
 // Handle Costume update form on PUT.
-exports.shoe_update_put = function(req, res) {
-    res.send('NOT IMPLEMENTED: Shoe update PUT' + req.params.id);
+exports.shoe_update_put = async function(req, res) {
+    console.log(`update on id ${req.params.id} with body ${JSON.stringify(req.body)}`)
+    try {
+        let toUpdate = await Shoe.findById(req.params.id)
+            // Do updates of properties
+        if (req.body.shoeCategory) toUpdate.shoeCategory = req.body.shoeCategory;
+        if (req.body.brand) toUpdate.brand = req.body.brand;
+        if (req.body.size) toUpdate.size = req.body.size;
+        let result = await toUpdate.save();
+        console.log("Sucess " + result)
+        res.send(result)
+    } catch (err) {
+        res.status(500)
+        res.send(`{"error": ${err}: Update for id ${req.params.id} failed`);
+    }
+
 };
 
 // VIEWS
