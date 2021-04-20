7111b9ba71fca6d5d9b439df08d915fb4be4166c
Raghavendra Reddy <77422478+reddy-raghavendra@users.noreply.github.com>
2021-04-19 23:14:08 -0500

    Lab 13

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 const connectionString = process.env.MONGO_CON
 mongoose = require('mongoose');
 mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -64,6 +65,26 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+passport.use(new LocalStrategy(
+    function(username, password, done) {
+        Account.findOne({ username: username }, function(err, user) {
+            if (err) { return done(err); }
+            if (!user) {
+                return done(null, false, { message: 'Incorrect username.' });
+            }
+            if (!user.validPassword(password)) {
+                return done(null, false, { message: 'Incorrect password.' });
+            }
+            return done(null, user);
+        });
+    }));
+app.use(require('express-session')({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -73,13 +94,18 @@ app.use('/stars', starsRouter);
 app.use('/slot', slotRouter);
 app.use('/', resourceRouter);
 
+// passport config
+// Use the existing connection
+// The Account model
+var Account = require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
     next(createError(404));
 });
-
-
-
 // error handler
 app.use(function(err, req, res, next) {
     // set locals, only providing error in development
